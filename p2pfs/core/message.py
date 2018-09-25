1deba980bf08a7c4112974ff5d71342f380871d8
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-09-25 10:12:24 -0400

    Use simple enum class to represent message types.

--- p2pfs/core/message.py
+++ p2pfs/core/message.py
@@ -1,31 +1,4 @@
-import socket
-
-MessageType = {
-    # peer -> server
-    'REQUEST_REGISTER': 1,
-    'REQUEST_FILE_LIST': 2,
-    'REQUEST_FILE_LOCATION': 3,
-    'REQUEST_CHUNK_REGISTER': 4,
-    'REQUEST_LEAVE': 5,
-
-    # server -> peer
-    'REPLY_REGISTER': 6,
-    'REPLY_FILE_LIST': 7,
-    'REPLY_FILE_LOCATION': 8,
-    'REPLY_CHUNK_REGISTER': 9,
-    'REPLY_LEAVE': 10,
-
-    # peer <-> peer
-    'PEER_REQUEST_CHUNK': 11,
-    'PEER_REPLY_CHUNK': 12
-}
-
-
-def write_message(s, message):
-    assert isinstance(s, socket.socket)
-    # TODO
-
-
-def read_message(s):
-    assert isinstance(s, socket.socket)
-    # TODO
+class MessageType:
+    REQUEST_REGISTER, REQUEST_FILE_LIST, REQUEST_FILE_LOCATION, REQUEST_CHUNK_REGISTER, REQUEST_LEAVE, \
+    REPLY_PEER_LIST, REPLY_REGISTER, REPLY_FILE_LIST, REPLY_FILE_LOCATION, REPLY_CHUNK_REGISTER, REPLY_LEAVE, \
+    PEER_REQUEST_CHUNK, PEER_REPLY_CHUNK = range(13)
