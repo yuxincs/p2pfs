993b1050de09227bd3cca96799f354ad02ab8e62
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-09-25 14:27:56 -0400

    Store id in server.

--- p2pfs/core/server.py
+++ p2pfs/core/server.py
@@ -8,11 +8,12 @@ class CentralServer(MessageServer):
     def __init__(self, host, port):
         super().__init__(host, port)
         self._peers = {}
+        self._id = 0
 
     def _client_connected(self, client):
         assert isinstance(client, socket.socket)
-        self._peers[client.getpeername()] = client
-        logger.debug(self._peers.keys())
+        self._peers[client] = None
+        logger.debug(self._peers.values())
 
     def _process_message(self, client, message):
         assert isinstance(client, socket.socket)
@@ -20,5 +21,5 @@ class CentralServer(MessageServer):
 
     def _client_closed(self, client):
         assert isinstance(client, socket.socket)
-        del self._peers[client.getpeername()]
-        logger.debug(self._peers.keys())
+        del self._peers[client]
+        logger.debug(self._peers.values())
