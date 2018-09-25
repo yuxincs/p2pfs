a7857d257d8af4928a3cbdebdc9fcfe549d9e0c0
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-09-25 09:59:57 -0400

    Use connect method of MessageServer.

--- p2pfs/core/peer.py
+++ p2pfs/core/peer.py
@@ -8,11 +8,9 @@ class PeerServer(MessageServer):
     def __init__(self, host, port, server, server_port):
         super().__init__(host, port)
         self._peers = {}
-        self._server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self._server_sock.connect((server, server_port))
-            logger.info('Successfully connected to server on {}'.format(self._server_sock.getsockname()))
-        except ConnectionRefusedError as e:
+            self._server_sock = self._connect(server, server_port)
+        except ConnectionRefusedError:
             logger.error('Server connection refused!')
             exit(1)
 
