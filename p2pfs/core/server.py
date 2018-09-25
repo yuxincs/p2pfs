291d7cbe9ef271e06020bca9c87356c67c081d5f
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-09-25 15:45:02 -0400

    Use uuid instead of serial number.

--- p2pfs/core/server.py
+++ p2pfs/core/server.py
@@ -2,6 +2,7 @@ from p2pfs.core.utils import MessageServer
 from p2pfs.core.message import MessageType
 import socket
 import logging
+import uuid
 logger = logging.getLogger(__name__)
 
 
@@ -9,7 +10,6 @@ class CentralServer(MessageServer):
     def __init__(self, host, port):
         super().__init__(host, port)
         self._peers = {}
-        self._id = 0
 
     def _client_connected(self, client):
         assert isinstance(client, socket.socket)
@@ -20,13 +20,13 @@ class CentralServer(MessageServer):
         assert isinstance(client, socket.socket)
         if message['type'] == MessageType.REQUEST_REGISTER:
             assert client in self._peers
+            id = str(uuid.uuid4())
             self._write_message(client, {
                 'type': MessageType.REPLY_REGISTER,
-                'id': self._id,
+                'id': id,
                 'peer_list': tuple(filter(lambda x: x is not None, self._peers.values()))
             })
-            self._peers[client] = (self._id, message['address'])
-            self._id += 1
+            self._peers[client] = (id, message['address'])
             logger.debug(self._peers.values())
 
     def _client_closed(self, client):
