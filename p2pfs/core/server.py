4575b6375311dddab725d408810aac2b26c09349
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-09-24 21:37:10 -0400

    Initial commit of CentralServer.
    Print the message it receives.

---
+++ p2pfs/core/server.py
@@ -0,0 +1,19 @@
+from p2pfs.core.utils import MessageServer
+import socket
+
+
+class CentralServer(MessageServer):
+    def __init__(self, host, port):
+        super().__init__(host, port)
+        self._peers = {}
+
+    def _client_connected(self, client, address):
+        assert isinstance(client, socket.socket)
+        print('{} connected!'.format(address))
+
+    def _process_message(self, client, message):
+        assert isinstance(client, socket.socket)
+        print(message)
+
+    def _client_closed(self, client):
+        pass
