09d7e39d9cf00baf44321812cf5fd856d249ddd1
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-10-06 09:52:22 -0400

    Simplify tracker based on MessageServer and asyncio.

--- p2pfs/core/tracker.py
+++ p2pfs/core/tracker.py
@@ -2,6 +2,7 @@ from p2pfs.core.server import MessageServer, MessageType
 import socket
 import logging
 import json
+import asyncio
 logger = logging.getLogger(__name__)
 
 
@@ -23,66 +24,61 @@ class Tracker(MessageServer):
     def peers(self):
         return tuple(self._peers.values())
 
-    def _client_connected(self, client):
-        assert isinstance(client, socket.socket)
-        self._peers[client] = None
-        logger.debug(self._peers.values())
-
-    def _process_message(self, client, message):
-        assert isinstance(client, socket.socket)
-        if message['type'] == MessageType.REQUEST_REGISTER:
-            assert client in self._peers
-            # peer_address is a string, since JSON requires keys being strings
-            self._peers[client] = json.dumps(message['address'])
-            logger.debug(self._peers.values())
-        elif message['type'] == MessageType.REQUEST_PUBLISH:
-            if message['filename'] in self._file_list:
-                self._write_message(client, {
-                    'type': MessageType.REPLY_PUBLISH,
-                    'filename': message['filename'],
-                    'result': False,
-                    'message': 'Filename already existed on server!'
+    async def _process_connection(self, reader, writer):
+        assert isinstance(reader, asyncio.StreamReader) and isinstance(writer, asyncio.StreamWriter)
+        self._peers[writer] = None
+        while not reader.at_eof():
+            message = await self._read_message(reader)
+            type = MessageType(message['type'])
+            if type == MessageType.REQUEST_REGISTER:
+                # peer_address is a string, since JSON requires keys being strings
+                self._peers[writer] = json.dumps(message['address'])
+                logger.debug(self._peers.values())
+            elif type == MessageType.REQUEST_PUBLISH:
+                if message['filename'] in self._file_list:
+                    await self._write_message(writer, {
+                        'type': MessageType.REPLY_PUBLISH,
+                        'filename': message['filename'],
+                        'result': False,
+                        'message': 'Filename already existed on server!'
+                    })
+                else:
+                    self._file_list[message['filename']] = message['fileinfo']
+                    # add to chunkinfo
+                    # TODO: optimize how the chunknums are stored
+                    self._chunkinfo[message['filename']] = {
+                        self._peers[writer]: list(range(0, message['chunknum']))
+                    }
+                    await self._write_message(writer, {
+                        'type': MessageType.REPLY_PUBLISH,
+                        'filename': message['filename'],
+                        'result': True,
+                        'message': 'Success'
+                    })
+                    logger.info('{} published file {} of {} chunks'
+                                .format(self._peers[writer], message['filename'], message['chunknum']))
+            elif type == MessageType.REQUEST_FILE_LIST:
+                await self._write_message(writer, {
+                    'type': MessageType.REPLY_FILE_LIST,
+                    'file_list': self._file_list
                 })
-            else:
-                self._file_list[message['filename']] = message['fileinfo']
-                # add to chunkinfo
-                # TODO: optimize how the chunknums are stored
-                self._chunkinfo[message['filename']] = {
-                    self._peers[client]: list(range(0, message['chunknum']))
-                }
-                self._write_message(client, {
-                    'type': MessageType.REPLY_PUBLISH,
+            elif type == MessageType.REQUEST_FILE_LOCATION:
+                await self._write_message(writer, {
+                    'type': MessageType.REPLY_FILE_LOCATION,
                     'filename': message['filename'],
-                    'result': True,
-                    'message': 'Success'
+                    'fileinfo': self._file_list[message['filename']],
+                    'chunkinfo': self._chunkinfo[message['filename']]
                 })
-                logger.info('{} published file {} of {} chunks'
-                            .format(self._peers[client], message['filename'], message['chunknum']))
-        elif message['type'] == MessageType.REQUEST_FILE_LIST:
-            self._write_message(client, {
-                'type': MessageType.REPLY_FILE_LIST,
-                'file_list': self._file_list
-            })
-        elif message['type'] == MessageType.REQUEST_FILE_LOCATION:
-            self._write_message(client, {
-                'type': MessageType.REPLY_FILE_LOCATION,
-                'filename': message['filename'],
-                'fileinfo': self._file_list[message['filename']],
-                'chunkinfo': self._chunkinfo[message['filename']]
-            })
-        elif message['type'] == MessageType.REQUEST_CHUNK_REGISTER:
-            peer_address = self._peers[client]
-            # TODO: merge the chunknum with the list
-            if peer_address in self._chunkinfo[message['filename']]:
-                self._chunkinfo[message['filename']][peer_address].append(message['chunknum'])
+            elif type == MessageType.REQUEST_CHUNK_REGISTER:
+                peer_address = self._peers[writer]
+                # TODO: merge the chunknum with the list
+                if peer_address in self._chunkinfo[message['filename']]:
+                    self._chunkinfo[message['filename']][peer_address].append(message['chunknum'])
+                else:
+                    self._chunkinfo[message['filename']][peer_address] = [message['chunknum']]
             else:
-                self._chunkinfo[message['filename']][peer_address] = [message['chunknum']]
-        else:
-            logger.error('Undefined message with {} type, full packet: {}'.format(message['type'], message))
+                logger.error('Undefined message with {} type, full packet: {}'.format(message['type'], message))
 
-    def _client_closed(self, client):
-        # TODO: hanlde client closed unexpectedly
-        logger.warning('{} closed'.format(client.getpeername()))
-        assert isinstance(client, socket.socket)
-        del self._peers[client]
-        logger.debug(self._peers.values())
+        writer.close()
+        del self._peers[writer]
+        await writer.wait_closed()
