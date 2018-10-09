990703b18db5bdab3a5f0b6e9b248d75487222cb
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-10-09 16:54:30 -0400

    Remove LEAVE message and add PEER_PING_PONG message type.

--- p2pfs/core/server.py
+++ p2pfs/core/server.py
@@ -14,14 +14,13 @@ class MessageType(Enum):
     REQUEST_FILE_LIST = auto()
     REQUEST_FILE_LOCATION = auto()
     REQUEST_CHUNK_REGISTER = auto()
-    REQUEST_LEAVE = auto()
     REPLY_REGISTER = auto()
     REPLY_FILE_LIST = auto()
     REPLY_PUBLISH = auto()
     REPLY_FILE_LOCATION = auto()
-    REPLY_LEAVE = auto()
     PEER_REQUEST_CHUNK = auto()
     PEER_REPLY_CHUNK = auto()
+    PEER_PING_PONG = auto()
 
 
 class MessageServer:
