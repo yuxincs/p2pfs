a74e7bc6beeb552f7f87bd5621e77cee0ff1b451
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-10-10 00:05:24 -0400

    Put stop tracker and peer into finally block.

--- tests/test_core.py
+++ tests/test_core.py
@@ -33,15 +33,17 @@ async def test_publish_refuse(unused_tcp_port):
     # spawn peers concurrently
     peers_started = await asyncio.gather(*[peer.start() for peer in peers])
     assert tracker_started and all(peers_started)
-    with open('test_publish_refuse', 'wb') as fout:
-        fout.write(os.urandom(100))
-    is_success, _ = await peers[0].publish('test_publish_refuse')
-    assert is_success
-    is_success, _ = await peers[0].publish('test_publish_refuse')
-    assert not is_success
-    os.remove('test_publish_refuse')
-    await tracker.stop()
-    await asyncio.gather(*[peer.stop() for peer in peers])
+    try:
+        with open('test_publish_refuse', 'wb') as fout:
+            fout.write(os.urandom(100))
+        is_success, _ = await peers[0].publish('test_publish_refuse')
+        assert is_success
+        is_success, _ = await peers[0].publish('test_publish_refuse')
+        assert not is_success
+        os.remove('test_publish_refuse')
+    finally:
+        await tracker.stop()
+        await asyncio.gather(*[peer.stop() for peer in peers])
 
 
 async def test_publish(unused_tcp_port):
@@ -50,25 +52,26 @@ async def test_publish(unused_tcp_port):
     tracker_started = await tracker.start()
     peer_started = await asyncio.gather(*[peer.start() for peer in peers])
     assert tracker_started and peer_started
+    try:
+        # peer0 publishes a small_file and peer1 publishes a large file
+        is_success, _ = await peers[0].publish(TEST_SMALL_FILE)
+        assert is_success
+        file_list = tracker.file_list()
+        assert TEST_SMALL_FILE in file_list
+        assert file_list[TEST_SMALL_FILE]['size'] == TEST_SMALL_FILE_SIZE
+        file_list = await peers[1].list_file()
+        assert TEST_SMALL_FILE in file_list
 
-    # peer0 publishes a small_file and peer1 publishes a large file
-    is_success, _ = await peers[0].publish(TEST_SMALL_FILE)
-    assert is_success
-    file_list = tracker.file_list()
-    assert TEST_SMALL_FILE in file_list
-    assert file_list[TEST_SMALL_FILE]['size'] == TEST_SMALL_FILE_SIZE
-    file_list = await peers[1].list_file()
-    assert TEST_SMALL_FILE in file_list
-
-    is_success, _ = await peers[1].publish(TEST_LARGE_FILE)
-    assert is_success
-    file_list = tracker.file_list()
-    assert TEST_LARGE_FILE in file_list and TEST_SMALL_FILE in file_list
-    assert file_list[TEST_LARGE_FILE]['size'] == TEST_LARGE_FILE_SIZE
-    file_list = await peers[0].list_file()
-    assert TEST_LARGE_FILE in file_list and TEST_SMALL_FILE in file_list
-    await tracker.stop()
-    await asyncio.gather(*[peer.stop() for peer in peers])
+        is_success, _ = await peers[1].publish(TEST_LARGE_FILE)
+        assert is_success
+        file_list = tracker.file_list()
+        assert TEST_LARGE_FILE in file_list and TEST_SMALL_FILE in file_list
+        assert file_list[TEST_LARGE_FILE]['size'] == TEST_LARGE_FILE_SIZE
+        file_list = await peers[0].list_file()
+        assert TEST_LARGE_FILE in file_list and TEST_SMALL_FILE in file_list
+    finally:
+        await tracker.stop()
+        await asyncio.gather(*[peer.stop() for peer in peers])
 
 
 async def test_download(unused_tcp_port):
@@ -212,15 +215,15 @@ async def test_peer_disconnect(unused_tcp_port):
     tracker_started = await tracker.start()
     peer_started = await peer.start()
     assert tracker_started and peer_started
+    try:
+        is_suceess, _ = await peer.publish(TEST_SMALL_FILE)
+        assert is_suceess
+        assert TEST_SMALL_FILE in tracker.file_list()
 
-    is_suceess, _ = await peer.publish(TEST_SMALL_FILE)
-    assert is_suceess
-    assert TEST_SMALL_FILE in tracker.file_list()
-
-    # stop peer and check the file has been removed
-    await peer.stop()
-    # return control to the loop for tracker code to run
-    await asyncio.sleep(1)
-    assert TEST_SMALL_FILE not in tracker.file_list()
-
-    await tracker.stop()
+        # stop peer and check the file has been removed
+        await peer.stop()
+        # return control to the loop for tracker code to run
+        await asyncio.sleep(1)
+        assert TEST_SMALL_FILE not in tracker.file_list()
+    finally:
+        await tracker.stop()
