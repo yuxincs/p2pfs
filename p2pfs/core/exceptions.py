7bf2d407db8da20e0274587dd98c53fdfb377193
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-10-13 16:26:22 -0400

    Add new types of exception.

--- p2pfs/core/exceptions.py
+++ p2pfs/core/exceptions.py
@@ -1,3 +1,17 @@
 class DownloadIncompleteError(EOFError):
-    def __init__(self, chunknum):
+    def __init__(self, message, chunknum):
+        super().__init__(message)
         self.chunknum = chunknum
+
+
+class AlreadyConnectedError(ConnectionError):
+    def __init__(self, address):
+        self.address = address
+
+
+class TrackerNotConnectedError(ConnectionError):
+    pass
+
+
+class InProgressError(Exception):
+    pass
