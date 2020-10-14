ec6e4e82d04feae4be3bcc5d66e7ed75d373e5ef
Yuxin Wang <yuxinwang.dev@gmail.com>
2020-10-14 13:02:52 -0400

    Use pbjson instead of json for object serialization.
    pbjson supports bytes, therefore eliminating the need for using pybase64 to transcode bytes to ascii string and use zstandard to compress it.

--- p2pfs/core/message.py
+++ p2pfs/core/message.py
@@ -2,11 +2,8 @@ from enum import Enum, auto
 import logging
 import asyncio
 import struct
-import json
-import zstandard as zstd
+import pbjson
 
-_compressor = zstd.ZstdCompressor()
-_decompressor = zstd.ZstdDecompressor()
 logger = logging.getLogger(__name__)
 
 
@@ -33,12 +30,12 @@ def _message_log(message):
 
 async def read_message(reader):
     assert isinstance(reader, asyncio.StreamReader)
-    # receive length header -> decompress (bytes) -> decode to str (str) -> json load (dict)
+    # receive length header -> pbjson load (dict)
     raw_msg_len = await reader.readexactly(4)
     msglen = struct.unpack('>I', raw_msg_len)[0]
     raw_msg = await reader.readexactly(msglen)
 
-    msg = json.loads(_decompressor.decompress(raw_msg).decode('utf-8'))
+    msg = pbjson.loads(raw_msg)
     logger.debug('Message received {}'.format(_message_log(msg)))
     return msg
 
@@ -49,10 +46,8 @@ async def write_message(writer, message):
     # use value of enum since Enum is not JSON serializable
     if isinstance(message['type'], MessageType):
         message['type'] = message['type'].value
-    # json string (str) -> encode to utf8 (bytes) -> compress (bytes) -> add length header (bytes)
-    raw_msg = json.dumps(message).encode('utf-8')
-    compressed = _compressor.compress(raw_msg)
-    logger.debug('Compression rate: {}'.format(len(compressed) / len(raw_msg)))
-    compressed = struct.pack('>I', len(compressed)) + compressed
-    writer.write(compressed)
+    # pbjson string (bytes) -> add length header (bytes)
+    raw_msg = pbjson.dumps(message)
+    raw_msg = struct.pack('>I', len(raw_msg)) + raw_msg
+    writer.write(raw_msg)
     await writer.drain()
